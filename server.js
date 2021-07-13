edfbf610265a094e439c0d07275c5f5871a0fa73
Kirk Clarke <kclarke.design@gmail.com>
2021-07-13 01:25:35 -0400

    add simple local server & tidying up

---
+++ server.js
@@ -0,0 +1,68 @@
+var pkg = require("./package.json");
+var pkgPaths = pkg.config.paths;
+
+const http = require("http");
+const fs = require("fs");
+// const url = require("url");
+const path = require("path");
+// const express = require("express");
+// let app = express();
+const hostname = "localhost";
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  // parse URL
+  let pathname = req.url;
+
+  // modify GET requests for images to include public path
+  if (req.url.match("^/images")) pathname = pkgPaths.public + pathname;
+
+  console.log(`${req.method} ${pathname}`);
+
+  // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+  const ext =
+    path.parse(pathname).ext == "" ? ".html" : path.parse(pathname).ext;
+
+  // maps file extention to MIME types
+  const map = {
+    ".ico": "image/x-icon",
+    ".html": "text/html",
+    ".js": "text/javascript",
+    ".json": "application/json",
+    ".css": "text/css",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".wav": "audio/wav",
+    ".mp3": "audio/mpeg",
+    ".svg": "image/svg+xml",
+    ".pdf": "application/pdf",
+    ".doc": "application/msword",
+  };
+
+  fs.stat(pathname, function (err, stats) {
+    if (err) {
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+      return;
+    }
+
+    if (fs.statSync(pathname).isDirectory())
+      pathname = pkgPaths.public + pathname + "index.html";
+
+    fs.readFile(pathname, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.writeHead(500);
+        res.end(err);
+        return;
+      }
+      res.setHeader("Content-Type", map[ext] || "text/plain");
+      res.writeHead(200);
+      res.end(data);
+    });
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server is listening at http://${hostname}:${port}/`);
+});
