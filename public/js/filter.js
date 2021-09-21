7d39c7d3c434de9e9e9e080b859b105dd07bb9d4
Kirk Clarke <kclarke.design@gmail.com>
2021-09-20 23:15:35 -0400

    started script for category filter

---
+++ public/js/filter.js
@@ -0,0 +1,8 @@
+const FILTER_SELECTOR = "js-filter";
+const CATEGORY_PARENT_SELECTOR = "js-cat-container";
+
+const getCategories = () => {
+  const categoryParentElems = document.querySelectorAll(
+    `.${CATEGORY_PARENT_SELECTOR}`
+  );
+};
