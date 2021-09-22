97ea5ab27e50e5a29d15e15042e5c89f12a36a7f
Kirk Clarke <kclarke.design@gmail.com>
2021-09-22 00:44:12 -0400

    working basic filtering logic added

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,5 +1,9 @@
 const FILTER_SELECTOR = "js-filter";
 const CATEGORY_PARENT_SELECTOR = "js-cat-container";
+const FILTER_SELECTED_SELECTOR = "filter-selected";
+const FILTERING_ACTIVE_SELECTOR = "filters-active";
+const projectGrid = document.querySelector("o-grid--projects");
+const projectTiles = document.querySelectorAll(".project-tile");
 
 const getCategories = () => {
   const catArray = [];
@@ -31,8 +35,6 @@ const addProjectCategories = () => {
   });
 };
 
-const allCategories = getCategories();
-
 const addFilters = (arr) => {
   const filterParent = document.querySelector(`.${FILTER_SELECTOR}`);
   filterParent.innerHTML = arr
@@ -40,7 +42,42 @@ const addFilters = (arr) => {
       return `<button class="btn btn-outline-secondary m-1" data-filter="${item}">${item}</button>`;
     })
     .join("");
+
+  for (filter of filterParent.children) {
+    filter.addEventListener("click", (e) => {
+      handleFilter(e);
+    });
+  }
+};
+
+const handleFilter = (e) => {
+  const activeFilters = [];
+  const filter = e.target;
+  const targetClass = filter.dataset.filter.toLowerCase();
+  const checkTiles = (action, cssClass) => {
+    projectTiles.forEach((tile) => {
+      if (action == "add") {
+        if (!tile.classList.value.includes(targetClass)) {
+          tile.classList.add(cssClass);
+        }
+      } else {
+        if (tile.classList.value.includes(targetClass)) {
+          tile.classList.remove(cssClass);
+        }
+      }
+    });
+  };
+  filter.classList.toggle(FILTER_SELECTED_SELECTOR);
+
+  if (filter.classList.value.toLowerCase().includes(FILTER_SELECTED_SELECTOR)) {
+    // add filter to activeFilters
+    checkTiles("add", "filtered-out");
+  } else {
+    // remove filter to activeFilters
+    checkTiles("remove", "filtered-out");
+    projectTiles.forEach((tile) => {});
+  }
 };
 
 addProjectCategories();
-addFilters(allCategories);
+addFilters(getCategories());
